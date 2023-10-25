e46e2511c3398bba0f630c73a7523ba27db6b9f5
Vahlgreen <vahlgreen@live.dk>
2023-10-25 19:48:30 +0200

    igang igen

--- TradingBot.py
+++ TradingBot.py
@@ -7,11 +7,13 @@ from datetime import datetime
 from datetime import timedelta
 
 
-def main():
-    test = 0
+# CREATE TICKER INSTANCE FOR AMAZON
+amzn = yf.Ticker("AMZN")
 
 
 
+# GET TODAYS DATE AND CONVERT IT TO A STRING WITH YYYY-MM-DD FORMAT (YFINANCE EXPECTS THAT FORMAT)
+end_date = datetime.now().strftime('%Y-%m-%d')
+amzn_hist = amzn.history(start='2022-01-01',end=end_date)
+print(amzn_hist)
 
-if(__name__ ==  "__main__"):
-    main()
--- stockpricecheck.py
+++ stockpricecheck.py
@@ -21,13 +21,15 @@ from datetime import timedelta
 
 def main():
 
+    end_date = datetime.now().strftime('%Y-%m-%d')
+
     cash = pickle.load( open( "cashstack.p", "rb" ) )
     portfolio = pickle.load( open( "portfolio.p", "rb" ) )
 
-    #print(f"Current cash stack ${cash}\n")
+    print(f"Current cash stack ${cash}\n")
     print(f"Current portfolio\n {portfolio}\n")
 
-    #input("press any key to continue")
+    input("press any key to continue")
 
     exchange = pd.read_csv("tickersymbols.csv",nrows=400)["Name"] # 411 stocks
     cash,portfolio = DecideBuy(exchange, portfolio, cash)
@@ -74,92 +76,18 @@ def Sell(stock,portfolio,cash):
     return cash,portfolio
 
 def Buy(stock,portfolio,cash):
-    #print(yf.Ticker(stock).history(period="1d")["Open"][0])
-    price = float(yf.Ticker(stock).history(period="1d")["Open"][0])
-    if(price<=cash):
-        print(f"Buying {stock}")
-       # PlotStock(yf.Ticker(stock).history(period="120d"))
-        portfolio[stock]=datetime.now().strftime("%Y-%m-%d")
-        cash = cash-price
-        print(f"new portfolio\n {portfolio} \n new cashstack ${cash}")
-    
-    else:
-        print("Not enough cash to complete transaction")
-
-    return cash,portfolio
+    return
 
 
 
-def DecideBuy(exchange, portfolio, cash):
-    cash = cash/2
-    candidates = []
-    print("\nlooking for stocks to buy...")
-    for index,stock in enumerate(exchange):
-        if(stock not in portfolio.keys()):
-            index = index+1
-            print(f"Currently considering stock number {index}: {stock}")
-            stockprice_history_short = yf.Ticker(stock).history(period="10d")["Open"]
-            if(len(stockprice_history_short)== 0):
-                print("skipping...")
-                continue
+def DecideBuy(ticker:str)->bool:
 
-            #pd.to_numeric(stockprice_history_short, errors='coerce') maybe not necessary?
-            ma_low = sum(stockprice_history_short)/len(stockprice_history_short)
-            stockprice_history_long = yf.Ticker(stock).history(period="70d")["Open"]
-            ma_high = sum(stockprice_history_long)/len(stockprice_history_long)
-
-            if(ma_high<ma_low and stockprice_history_short[0]<cash/10 and stock not in portfolio):
-                candidates.append((stock,ma_low,ma_high,stockprice_history_short[0]))
-                print(f"added {stock} as candidate")
-
-    # Determine best candidates
-    sorted_candidates=sorted(candidates,key=lambda x:(x[1]-x[2])/x[3],reverse=True)
-
-    if len(sorted_candidates)>10:
-        for stock in sorted_candidates[:10]:
-            cash,portfolio = Buy(stock[0],portfolio,cash)
-    else:
-        for stock in sorted_candidates:
-            cash,portfolio = Buy(stock[0],portfolio,cash)
-
-    return cash,portfolio
+    return True
 
 
 
-def DecideSell(portfolio,cash):
-    print("\nlooking for stocks to sell...")
-    for stock in portfolio.keys():
-        history = yf.Ticker(stock).history(period="1y")["Open"]
-        #if stock has been held for at least 10 days we consider selling
-        if datetime.strptime(portfolio[stock],"%Y-%m-%d") < datetime.today()+timedelta(days=10):
-            bought_price = history[pd.Timestamp(portfolio[stock])-pd.Timedelta(days=2)]
-            todays_price =history[0]
-            if todays_price>bought_price:
-                Sell(stock,portfolio,cash)
-
-    return cash, portfolio
-    #bought_date = portfolio[stock]
-
-    #bought_price = history[bought_date]
-
-
-
-
-        # ma_low = sum(history_short)/len(history_short)
-        # history_long = yf.Ticker(stock).history(period="50d")["Open"]
-        # ma_high = sum(history_long)/len(history_long)
-        # if(ma_high<ma_low and history_short[0]<cash):
-        #     cash,portfolio = Sell(stock,portfolio,cash)
-
-
-
-
-    # An additional strategy is called the opportunity-cost sell method.
-    # In this method, the investor owns a portfolio of stocks and sells a stock when a better opportunity presents itself.
-    # This requires constant monitoring, research, and analysis of both their portfolio and potential new stock additions.
-    # Once a better potential investment has been identified, the investor then reduces or eliminates a position in a current
-    # holding that isn't expected to do as well as the new stock on a risk-adjusted return basis.
-
+def DecideSell():
+    return
 
 def ComputeSharpRatio():
     
