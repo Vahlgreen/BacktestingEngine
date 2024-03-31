d6e2df89653f8b874a45318bb96f32dd286028be
Vahlgreen <vahlgreen@live.dk>
2024-03-31 21:27:43 +0200

    started backtest implementation

--- TradingBot.py
+++ TradingBot.py
@@ -1,11 +1,9 @@
-import pandas
 import yfinance as yf
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
-import time
-import math
+import os
 
 class PortfolioObject:
     def __init__(self, curPrice: float, units: int,transactionDate: str):
@@ -15,27 +13,33 @@ class PortfolioObject:
 
     def GetTotalValue(self):
         return self.curPrice*self.units
-
 def DumpPortfolio(portfolio: dict) -> None:
-    #Todo: explicit path
-    with open("C:/Users/Vahlg/PycharmProjects/TradingBot/portfolio.p", "wb") as f:
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, "portfolio.p")
+    with open(data_file, "wb") as f:
         pickle.dump(portfolio,f)
         f.close()
 def LoadPortfolio() -> dict:
-    with  open("C:/Users/Vahlg/PycharmProjects/TradingBot/portfolio.p", "rb") as f:
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, "portfolio.p")
+    with  open(data_file, "rb") as f:
         portfolio = pickle.load(f)
         f.close()
     return portfolio
-def ResetPortfolio(val: dict = {}) -> None:
-    DumpPortfolio({})
+def ResetPortfolio(val=None) -> None:
+    if val is None:
+        val = {}
+    DumpPortfolio(val)
 def DumpFunds(funds: float) -> None:
-    #TODO:explicit path
-    with open("C:/Users/Vahlg/PycharmProjects/TradingBot/funds.p", "wb") as f:
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, "funds.p")
+    with open(data_file, "wb") as f:
         pickle.dump(funds,f)
         f.close()
 def LoadFunds() -> float:
-    # TODO:explicit path
-    with  open("C:/Users/Vahlg/PycharmProjects/TradingBot/funds.p", "rb") as f:
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, "funds.p")
+    with  open(data_file, "rb") as f:
         funds = pickle.load(f)
         f.close()
     return float(funds)
@@ -51,37 +55,58 @@ def BuyStock(portfolio: dict, funds: float, stockTicker: str, curPrice: float, n
         #Todo: errorhandling
         #raise Exception
         print("")
-def SellStock(portfolio: dict, funds: float, stockTicker: str, curPrice: float):
+def SellStock(portfolio: dict, funds: float, stockTicker: str, curPrice: float) -> None:
     if stockTicker in portfolio.keys():
-        funds = funds+curPrice
+        funds = funds+curPrice*portfolio[stockTicker].units()
         del portfolio[stockTicker]
     else:
         #Todo: errorhandling
         raise Exception
 def BackTestStrategy() -> None:
-    quit()
+    pass
 def MomentumBasedStrategy(stockHist: yf.ticker.Ticker.history,flag: str) -> bool:
-    # When the close crosses above the 25-day high of the close, we go long at the close.
-    # When the close crosses below the 25-day high of the close, we sell at the close.
-    period = 25
-    curMax = 0
-    if flag == "buy":
-        #For some reason list[-x:-1] does not contain the last element of the list. However, list[-1] returns exactly that
-        for obs in stockHist.iloc[-period+1:-1]:
-            curMax = max(curMax, obs)
-        return curMax < stockHist.iloc[-1]
-    elif flag == "sell":
-        for obs in stockHist.iloc[-period+1:-1]:
-            curMax = max(curMax, obs)
-        return curMax > stockHist.iloc[-1]
-    else:
-        raise Exception
-def SummarizeRun(portfolio: dict) -> None:
-    #Todo: perhaps make portfolio custom class?
-    totValue = GetPortfolioWorth(portfolio)
-    numStocks = len(portfolio.items())
 
-    print(f"Portfolio consists of {numStocks} stocks\nTotal portfolio value: {round(totValue,1)}")
+    if flag == "buy":
+        if stockHist.iloc[-1]>stockHist.iloc[-2]:
+            return True
+        else:
+            return False
+    else:
+        if stockHist.iloc[-1]<stockHist.iloc[-2]:
+            return True
+        else:
+            return False
+    # # When the close crosses above the 25-day high of the close, we go long at the close.
+    # # When the close crosses below the 25-day high of the close, we sell at the close.
+    # period = 25
+    # curMax = 0
+    # if flag == "buy":
+    #     #For some reason list[-x:-1] does not contain the last element of the list. However, list[-1] returns exactly that
+    #     for obs in stockHist.iloc[-period+1:-1]:
+    #         curMax = max(curMax, obs)
+    #     return curMax < stockHist.iloc[-1]
+    # elif flag == "sell":
+    #     for obs in stockHist.iloc[-period+1:-1]:
+    #         curMax = max(curMax, obs)
+    #     return curMax > stockHist.iloc[-1]
+    # else:
+    #     raise Exception
+def CreateBacktestOutput(portfolio: dict, funds: float,endDate: str) -> None:
+
+    #Todo: save primo numbers to file for monthly recap
+    #Todo: perhaps make portfolio custom class?
+
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, "BacktestResults.p")
+    with open(data_file, "rb") as f:
+        backTestResults = pickle.load(f)
+        f.close()
+
+    backTestResults.loc[len(backTestResults.index)] = [GetPortfolioWorth(portfolio), funds,datetime.date(endDate)]
+
+    with open(data_file, "wb") as f:
+        pickle.dump(backTestResults, f)
+        f.close()
 def GetPortfolioWorth(portfolio: dict) -> float:
     totValue = 0
     for key in portfolio.keys():
@@ -122,17 +147,15 @@ def ProcessStockPool(stockPool: dict, portfolio: dict,funds: float) -> None:
     for row in df.iterrows():
         series = row[1]
         BuyStock(portfolio,funds,series["Name"],series["Price"],series["UnitsToBuy"])
-def ValidateHistoryData(stockHist: yf.ticker.Ticker.history) -> bool:
+def ValidateHistoryData(stockHist: yf.ticker.Ticker.history,endDate: str) -> bool:
     if stockHist.empty:
         return False
-    elif stockHist.last_valid_index().strftime('%Y-%m-%d') < (datetime.today() - timedelta(weeks=1)).strftime('%Y-%m-%d'):
+    elif stockHist.last_valid_index().strftime('%Y-%m-%d') < (datetime.date(endDate) - timedelta(weeks=1)).strftime('%Y-%m-%d'):
         return False
     else:
         return True
+def Deploy(exchange: pd.DataFrame,endDate: str = datetime.now().strftime('%Y-%m-%d')) -> None:
 
-def Main():
-    #Todo: download sp500 ticker list every ?monday? and overwrite in disk
-    exchange = pd.read_csv("tickersymbols.csv", nrows=50)  #sp500
     stockPool = {
         "Name": [],
         "Price":[],
@@ -144,30 +167,23 @@ def Main():
     funds = LoadFunds()
     portfolio = LoadPortfolio()
 
-    if funds < 0:
-        print("No funds available. Terminating..")
-        raise Exception
-    else:
-        primoFunds = funds
-        #Todo: save primo numbers to file for monthly recap
-        #primoPortfolio = GetPortfolioWorth(portfolio)
-
+    #For statistics
+    #primoPortfolio = GetPortfolioWorth(portfolio)
     #loop through exchange
     for row in exchange.iterrows():
         stockTicker = row[1][0]
-
+        print(stockTicker)
         #Ticker object
         stockObject = yf.Ticker(stockTicker)
 
         #Date format is strict
-        endDate = datetime.now().strftime('%Y-%m-%d')
         stockHist = stockObject.history(start='2000-01-01', end=endDate)["Close"]
 
-        if not ValidateHistoryData(stockHist):
+        if not ValidateHistoryData(stockHist,endDate):
             continue
         curPrice = stockHist.iloc[-1]
 
-        #Evaluate strategy
+        #Deploy strategy
         if stockTicker in portfolio.keys():
             if MomentumBasedStrategy(stockHist, "sell"):
                 SellStock(portfolio, funds, curPrice, stockTicker)
@@ -175,10 +191,46 @@ def Main():
         else:
             if MomentumBasedStrategy(stockHist, "buy"):
                 #Todo: keep statistics on the amount of stocks passing the momentum criteria
+                print(f"Considering {stockTicker}")
                 PrepareStockPool(stockPool,stockTicker,curPrice,stockHist)
+    #If the pool is not empty we process and buy
+    if stockPool["Name"] != []:
+        ProcessStockPool(stockPool,portfolio,funds)
+        CreateBacktestOutput(portfolio,funds,endDate)
+    else:
+        print("No stocks found")
+def Backtest(exchange: pd.DataFrame,startDate: str, endDate: str) -> None:
+    #reset backtest results before run
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, "BacktestResults.p")
+    with open(data_file, "wb") as f:
+        pickle.dump(pd.DataFrame(columns=["TotalValue","Funds","TransactionDate"]),f)
+        f.close()
 
-    ProcessStockPool(stockPool,portfolio,funds)
-    SummarizeRun(portfolio)
+    dateIterable = pd.date_range(startDate,endDate,freq='d')
+    for date in dateIterable:
+        Deploy(exchange,endDate=date)
+
+def Main(deployOrBacktest: str) -> None:
+    #Find directory (explicitly, to avoid issues on pe server)
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(script_directory, "tickersymbols.csv")
+
+    #on deployment, we take all 500 stocks and deploy the algorithm
+    exchange = pd.read_csv(data_file, nrows=250)  # sp500
+    #On backtest we choose one or more stocks to backtest the algorithm
+    backtestTickerList = pd.DataFrame({"Name":["AAPL","AMZN","SOUN","PEP","CRM","TSLA","NVDA"]})
+
+    if deployOrBacktest.lower() == "deploy":
+        # Todo: download sp500 ticker list every ?monday? and overwrite in disk
+        Deploy(exchange)
+
+    elif deployOrBacktest.lower() == "backtest":
+        exchange = backtestTickerList#exchange[exchange.apply(lambda x: True if x.TICKERS in backtestTickerList else False, axis=1)]
+        if exchange.empty:
+            raise Exception
+        Backtest(exchange,"01-01-2015",datetime.now().strftime('%Y-%m-%d'))
+
 
 #momentum strat:
 #When the close crosses above the 100-day high of the close, we go long at the close.
@@ -190,4 +242,5 @@ def Main():
 #Hold for one month and rinse and repeat (or continue being long the same instrument)
 
 if(__name__ ==  "__main__"):
-    Main()
+    #Main("Deploy")
+    Main("Backtest")
